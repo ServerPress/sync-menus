1f44c86fa8beb887a0d6abdc48f4c53bfdb7ac69
Dave Jesch <dave@davejesch.com>
2016-08-23 13:04:22 -0700

    change spaces to tabs; disable debug code

--- assets/js/sync-menus.js
+++ assets/js/sync-menus.js
@@ -9,9 +9,9 @@
 
 function WPSiteSyncContent_Menus()
 {
-    this.inited = false;
-    this.$content = null;
-    this.disable = false;
+	this.inited = false;
+	this.$content = null;
+	this.disable = false;
 }
 
 /**
@@ -19,29 +19,29 @@ function WPSiteSyncContent_Menus()
  */
 WPSiteSyncContent_Menus.prototype.init = function ()
 {
-    this.inited = true;
+	this.inited = true;
 
-    var _self = this,
-        target = document.querySelector('#menu-to-edit'),
-        observer = new MutationObserver(function (mutations)
-        {
-            mutations.forEach(function (mutation)
-            {
-                _self.on_content_change();
-            });
-        });
+	var _self = this,
+		target = document.querySelector('#menu-to-edit'),
+		observer = new MutationObserver(function (mutations)
+		{
+			mutations.forEach(function (mutation)
+			{
+				_self.on_content_change();
+			});
+		});
 
-    var config = {attributes: true, childList: true, characterData: true}
+	var config = {attributes: true, childList: true, characterData: true}
 
-    observer.observe(target, config);
+	observer.observe(target, config);
 
-    this.$content = jQuery('#update-nav-menu');
-    this.$content.on('keypress change', function ()
-    {
-        _self.on_content_change();
-    });
+	this.$content = jQuery('#update-nav-menu');
+	this.$content.on('keypress change', function ()
+	{
+		_self.on_content_change();
+	});
 
-    this.show();
+	this.show();
 };
 
 /**
@@ -49,10 +49,10 @@ WPSiteSyncContent_Menus.prototype.init = function ()
  */
 WPSiteSyncContent_Menus.prototype.show = function ()
 {
-    this.hide_msgs();
+	this.hide_msgs();
 
-    jQuery('#nav-menu-header .publishing-action').after(jQuery('#sync-menu-ui').html());
-    jQuery('#nav-menu-footer .publishing-action').after(jQuery('#sync-menu-ui').html());
+	jQuery('#nav-menu-header .publishing-action').after(jQuery('#sync-menu-ui').html());
+	jQuery('#nav-menu-footer .publishing-action').after(jQuery('#sync-menu-ui').html());
 };
 
 /**
@@ -61,10 +61,10 @@ WPSiteSyncContent_Menus.prototype.show = function ()
  */
 WPSiteSyncContent_Menus.prototype.hide_msgs = function ()
 {
-    jQuery('.sync-menu-msgs').hide();
-    jQuery('.sync-menu-loading-indicator').hide();
-    jQuery('.sync-menu-failure-msg').hide();
-    jQuery('.sync-menu-success-msg').hide();
+	jQuery('.sync-menu-msgs').hide();
+	jQuery('.sync-menu-loading-indicator').hide();
+	jQuery('.sync-menu-failure-msg').hide();
+	jQuery('.sync-menu-success-msg').hide();
 };
 
 /**
@@ -72,8 +72,8 @@ WPSiteSyncContent_Menus.prototype.hide_msgs = function ()
  */
 WPSiteSyncContent_Menus.prototype.on_content_change = function ()
 {
-    //console.log( 'content changed' );
-    this.disable = true;
+//console.log( 'content changed' );
+	this.disable = true;
 };
 
 /**
@@ -83,28 +83,28 @@ WPSiteSyncContent_Menus.prototype.on_content_change = function ()
  */
 WPSiteSyncContent_Menus.prototype.set_message = function (type, msg)
 {
-    if (!this.inited)
-        return;
+	if (!this.inited)
+		return;
 
-    jQuery('.sync-menu-msgs').show();
-    if ('loading' === type) {
-        jQuery('.sync-menu-loading-indicator').show();
-    } else if ('success' === type) {
-        jQuery('.sync-menu-success-msg').show();
-    } else if ('unsaved' === type) {
-        jQuery('.sync-menu-failure-unsaved').show();
-        jQuery('.sync-menu-failure-api').hide();
-        jQuery('.sync-menu-failure-msg').show();
-    } else if ('api' === type) {
-        jQuery('.sync-menu-failure-api').show();
-        jQuery('.sync-menu-failure-unsaved').hide();
-        jQuery('.sync-menu-failure-msg').show();
-    } else {
-        jQuery('.sync-menu-failure-detail').html(msg);
-        jQuery('.sync-menu-failure-api').hide();
-        jQuery('.sync-menu-failure-unsaved').hide();
-        jQuery('.sync-menu-failure-msg').show();
-    }
+	jQuery('.sync-menu-msgs').show();
+	if ('loading' === type) {
+		jQuery('.sync-menu-loading-indicator').show();
+	} else if ('success' === type) {
+		jQuery('.sync-menu-success-msg').show();
+	} else if ('unsaved' === type) {
+		jQuery('.sync-menu-failure-unsaved').show();
+		jQuery('.sync-menu-failure-api').hide();
+		jQuery('.sync-menu-failure-msg').show();
+	} else if ('api' === type) {
+		jQuery('.sync-menu-failure-api').show();
+		jQuery('.sync-menu-failure-unsaved').hide();
+		jQuery('.sync-menu-failure-msg').show();
+	} else {
+		jQuery('.sync-menu-failure-detail').html(msg);
+		jQuery('.sync-menu-failure-api').hide();
+		jQuery('.sync-menu-failure-unsaved').hide();
+		jQuery('.sync-menu-failure-msg').show();
+	}
 };
 
 /**
@@ -113,41 +113,41 @@ WPSiteSyncContent_Menus.prototype.set_message = function (type, msg)
  */
 WPSiteSyncContent_Menus.prototype.push_menu = function (menu_name)
 {
-    //console.log('PUSH' + menu_name);
+//console.log('PUSH' + menu_name);
 
-    // Do nothing when in a disabled state
-    if (this.disable || !this.inited)
-        return;
+	// Do nothing when in a disabled state
+	if (this.disable || !this.inited)
+		return;
 
-    var data = {
-        action: 'spectrom_sync',
-        operation: 'pushmenu',
-        menu_name: menu_name,
-        _sync_nonce: jQuery('#_sync_nonce').val()
-    };
+	var data = {
+		action: 'spectrom_sync',
+		operation: 'pushmenu',
+		menu_name: menu_name,
+		_sync_nonce: jQuery('#_sync_nonce').val()
+	};
 
-    wpsitesynccontent.menus.hide_msgs();
-    wpsitesynccontent.menus.set_message('loading');
+	wpsitesynccontent.menus.hide_msgs();
+	wpsitesynccontent.menus.set_message('loading');
 
-    jQuery.ajax({
-        type: 'post',
-        async: true, // false,
-        data: data,
-        url: ajaxurl,
-        success: function (response)
-        {
-            wpsitesynccontent.menus.hide_msgs();
-            //console.log('in ajax success callback - response');
-            console.log(response);
-            if (response.success) {
-                wpsitesynccontent.menus.set_message('success');
-            } else if (0 !== response.error_code) {
-                wpsitesynccontent.menus.set_message('failure', response.error_message);
-            } else {
-                wpsitesynccontent.menus.set_message('api');
-            }
-        }
-    });
+	jQuery.ajax({
+		type: 'post',
+		async: true, // false,
+		data: data,
+		url: ajaxurl,
+		success: function (response)
+		{
+			wpsitesynccontent.menus.hide_msgs();
+//console.log('in ajax success callback - response');
+console.log(response);
+			if (response.success) {
+				wpsitesynccontent.menus.set_message('success');
+			} else if (0 !== response.error_code) {
+				wpsitesynccontent.menus.set_message('failure', response.error_message);
+			} else {
+				wpsitesynccontent.menus.set_message('api');
+			}
+		}
+	});
 };
 
 /**
@@ -156,42 +156,42 @@ WPSiteSyncContent_Menus.prototype.push_menu = function (menu_name)
  */
 WPSiteSyncContent_Menus.prototype.pull_menu = function (menu_name)
 {
-    //console.log('PULL' + menu_name);
+//console.log('PULL' + menu_name);
 
-    // Do nothing when in a disabled state
-    if (this.disable || !this.inited)
-        return;
+	// Do nothing when in a disabled state
+	if (this.disable || !this.inited)
+		return;
 
-    var data = {
-        action: 'spectrom_sync',
-        operation: 'pullmenu',
-        menu_name: menu_name,
-        _sync_nonce: jQuery('#_sync_nonce').val()
-    };
+	var data = {
+		action: 'spectrom_sync',
+		operation: 'pullmenu',
+		menu_name: menu_name,
+		_sync_nonce: jQuery('#_sync_nonce').val()
+	};
 
-    wpsitesynccontent.menus.hide_msgs();
-    wpsitesynccontent.menus.set_message('loading');
+	wpsitesynccontent.menus.hide_msgs();
+	wpsitesynccontent.menus.set_message('loading');
 
-    jQuery.ajax({
-        type: 'post',
-        async: true, // false,
-        data: data,
-        url: ajaxurl,
-        success: function (response)
-        {
-            wpsitesynccontent.menus.hide_msgs();
-            // console.log('in ajax success callback - response');
-            //console.log(response);
-            if (response.success) {
-                wpsitesynccontent.menus.set_message('success');
-                location.reload();
-            } else if (0 !== response.error_code) {
-                wpsitesynccontent.menus.set_message('failure', response.error_message);
-            } else {
-                wpsitesynccontent.menus.set_message('api');
-            }
-        }
-    });
+	jQuery.ajax({
+		type: 'post',
+		async: true, // false,
+		data: data,
+		url: ajaxurl,
+		success: function (response)
+		{
+			wpsitesynccontent.menus.hide_msgs();
+//console.log('in ajax success callback - response');
+//console.log(response);
+			if (response.success) {
+				wpsitesynccontent.menus.set_message('success');
+				location.reload();
+			} else if (0 !== response.error_code) {
+				wpsitesynccontent.menus.set_message('failure', response.error_message);
+			} else {
+				wpsitesynccontent.menus.set_message('api');
+			}
+		}
+	});
 };
 
 wpsitesynccontent.menus = new WPSiteSyncContent_Menus();
@@ -199,23 +199,23 @@ wpsitesynccontent.menus = new WPSiteSyncContent_Menus();
 // initialize the WPSiteSync operation on page load
 jQuery(document).ready(function ()
 {
-    wpsitesynccontent.menus.init();
+	wpsitesynccontent.menus.init();
 
-    jQuery('.sync-menu-contents').on('click', '.sync-menus-push, .sync-menus-pull', function ()
-    {
-        var menu_name = jQuery('#menu-name').val();
+	jQuery('.sync-menu-contents').on('click', '.sync-menus-push, .sync-menus-pull', function ()
+	{
+		var menu_name = jQuery('#menu-name').val();
 
-        wpsitesynccontent.menus.hide_msgs();
+		wpsitesynccontent.menus.hide_msgs();
 
-        if (!menu_name || '' === menu_name || true === wpsitesynccontent.menus.disable) {
-            wpsitesynccontent.menus.set_message('unsaved');
-            return;
-        }
+		if (!menu_name || '' === menu_name || true === wpsitesynccontent.menus.disable) {
+			wpsitesynccontent.menus.set_message('unsaved');
+			return;
+		}
 
-        if (jQuery(this).hasClass('sync-menus-pull')) {
-            wpsitesynccontent.menus.pull_menu(menu_name);
-        } else if (jQuery(this).hasClass('sync-menus-push')) {
-            wpsitesynccontent.menus.push_menu(menu_name);
-        }
-    });
+		if (jQuery(this).hasClass('sync-menus-pull')) {
+			wpsitesynccontent.menus.pull_menu(menu_name);
+		} else if (jQuery(this).hasClass('sync-menus-push')) {
+			wpsitesynccontent.menus.push_menu(menu_name);
+		}
+	});
 });
